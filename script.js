533e5460eb4c471c9f7b0dd3d18d8d11a5c45244
Raj Thakur <rajthakur2003@gmail.com>
2022-01-22 14:52:44 +0530

    Completed basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var principal = document.getElementById("principal").value;
+  if (principal == "0" || principal == "") {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    location.reload();
+    return false;
+  }
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = (principal * years * rate) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+  var amount = parseInt(principal) + interest;
+  document.getElementById("result").innerHTML =
+    "If you deposit  <mark>" +
+    principal +
+    "</mark>,<br>at an interest rate of  <mark>" +
+    rate +
+    "%</mark><br>You will receive an amount of  <mark>" +
+    amount +
+    "</mark>,<br>in the year  <mark>" +
+    year +
+    " </mark><br>";
 }
-        
+
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
+}
